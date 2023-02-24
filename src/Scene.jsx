329ced2cda559d85e8cf4d842be5eaa6cc0df2f6
Brandon <brandonkly@ucla.edu>
2023-02-23 22:14:32 -0800

    prepared canvas

---
+++ src/Scene.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function Scene() {
+
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default Scene
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import Scene from './Scene'
+import { Canvas } from '@react-three/fiber'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Canvas>
+    <Scene />
+  </Canvas>
 )
