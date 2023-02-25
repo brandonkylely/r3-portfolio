3b7d94c725e11290122a6ff1926ccaaa66ca468d
Brandon <brandonkly@ucla.edu>
2023-02-25 00:53:19 -0800

    added resume pdf

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -3,12 +3,13 @@ import React from 'react';
 export default function About({}) {
     return (
         <div className='grid grid-cols-2'>
-        <div className='text-right font-bold text-3xl mt-64 mr-28'>
+        <div className='flex justify-center items-end flex-col font-bold text-3xl mt-16 mr-48'>
+            <div style={{ backgroundImage: 'url(profile-picture1.jpg)'}} className='overflow-hidden bg-cover border-2 border-black rounded-full w-48 h-48 text-right'></div>
             my resume
             <br />
             ━━━━━━━━━━━━━
-            <p className='font-xl w-2/3 text-left '>
-
+            <p className='mr-36'>
+            <a href="brandon-resume.pdf" target="_blank" className='text-2xl'>PDF</a>
             </p>
         </div>
         <div className='font-bold text-3xl mt-12 col-start-2'>
