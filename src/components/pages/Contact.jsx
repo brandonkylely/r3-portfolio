c405493679669d267427efca1c07f476d3c65c59
Brandon <brandonkly@ucla.edu>
2023-02-25 01:09:56 -0800

    added contact form submission

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,9 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const formEndpoint = "https://public.herotofu.com/v1/5e6a9170-b4eb-11ed-a036-a589518453bd";
 
 export default function Contact({}) {
+    const [submitted, setSubmitted] = useState(false);
+    const handleSubmit = () => {
+      setTimeout(() => {
+        setSubmitted(true);
+      }, 100);
+    };
+  
+    if (submitted) {
+      return (
+        <>
+          <div className="text-2xl">Email sent...</div>
+          <div className="text-md">and email received...</div>
+        </>
+      );
+    }
+  
     return (
-        <div className='text-xl'>
-            contact
+      <form
+        action={formEndpoint}
+        onSubmit={handleSubmit}
+        method="POST"
+        target="_blank"
+      >
+        <div className="mb-3 pt-0">
+          <input
+            type="text"
+            placeholder="Your name"
+            name="name"
+            className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+            required
+          />
         </div>
-    )
-}
+        <div className="mb-3 pt-0">
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+            required
+          />
+        </div>
+        <div className="mb-3 pt-0">
+          <textarea
+            placeholder="Your message"
+            name="message"
+            className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+            required
+          />
+        </div>
+        <div className="mb-3 pt-0">
+          <button
+            className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+            type="submit"
+          >
+            Send a message
+          </button>
+        </div>
+      </form>
+    );
+  };
