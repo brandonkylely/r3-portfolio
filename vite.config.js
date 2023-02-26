3e8e88f3e22c3d05ee79c78f09cb17cbf55e8314
Brandon <brandonkly@ucla.edu>
2023-02-25 18:30:02 -0800

    vite config base fixed?

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/R3-portfolio/',
+  base: '/R3-Portfolio/',
   plugins: [react()],
 })
