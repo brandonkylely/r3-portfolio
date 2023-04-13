8f300531f27e46d6987e397449e4889bb8660d93
Brandon <brandonkly@ucla.edu>
2023-04-12 18:20:27 -0700

    added loading screen

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,2 +1,6 @@
+import { useProgress, Html } from "@react-three/drei"
+
 export default function Loading() {
+    const { active, progress, errors, item, loaded, total } = useProgress()
+    return <Html center>{progress} % loaded</Html>
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Scene from './Scene'
-import { Canvas } from '@react-three/fiber'
-import './index.css'
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import Scene from "./Scene";
+import Loading from "./components/Loading";
+import { Canvas } from "@react-three/fiber";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Canvas
-  camera={{
-    fov: 45,
-    near: 0.1,
-    far: 2000,
-    position: [ -0.5, 1.7, 3.2 ]
+    camera={{
+      fov: 45,
+      near: 0.1,
+      far: 2000,
+      position: [-0.5, 1.7, 3.2],
     }}
-    >
+  >
+  <Suspense fallback={<Loading />}> 
     <Scene />
+  </Suspense>
   </Canvas>
-)
+);
