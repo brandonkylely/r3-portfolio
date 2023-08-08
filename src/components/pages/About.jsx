641b35a2fe4f2a47cfba7a6945da63b12af2000a
Brandon <brandonkly@ucla.edu>
2023-08-07 23:29:25 -0700

    new about

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,39 +1,54 @@
-import React from 'react';
+import React from "react";
 
 export default function About({}) {
-    return (
-        <div className='grid grid-cols-2 overlayScreen'>
-        <div className='flex justify-center items-end flex-col font-bold text-3xl mt-16 mr-10'>
-            <div style={{ backgroundImage: 'url(profile-picture1.jpg)'}} className='mr-40 overflow-hidden bg-cover border-2 border-black rounded-full w-48 h-48 text-right'></div>
-            <div className='mr-28 text-right'>
-                Socials
-            <br />
-            ━━━━━━━━━━━━━━━━━━━━━
-            </div>
-            <div className='mr-20'>
-            <p className='text-xl'>━B.S. in Biochemistry, UCLA <span className='italic text-base'>2016-2020</span></p>
-            <p className='text-xl'>━Fullstack/Flex Coding, 
-            <br />
-            UCI Bootcamp <span className='italic text-base'>2022-2023</span></p>
-            <br />
-            <a href="brandon-resume.pdf" target="_blank" className='text-2xl'>Resume</a>
-            </div>
+  return (
+    <div className="grid grid-cols-2 overlayScreen">
+      <div className="flex justify-center items-end flex-col font-bold text-3xl mt-16 mr-10">
+        <div
+          style={{ backgroundImage: "url(profile-picture1.jpg)" }}
+          className="mr-40 overflow-hidden bg-cover border-2 border-black rounded-full w-48 h-48 text-right"
+        ></div>
+        <div className="mr-28 text-right">
+          Socials
+          <br />
+          ━━━━━━━━━━━━━━━━━━━━━
         </div>
-        <div className='font-bold text-3xl mt-12 col-start-2'>
-            About Me
-            <br />
-            ━━━━━━━━━━━━━━━━━━━━━━━━━━
-            <p className='font-xl w-2/3 text-right ml-12'>
-                Hi, I'm Brandon, and welcome to my portfolio.
-                <br />
-                <br />
-                I'm a Fullstack Software Engineer with a background in Biochemistry.
-                <br />
-                <br />
-                Feel free to contact me with any business inquiries!
-            </p>
+        <div className="mr-20 w-80 text-5xl ">
+          <a href="https://github.com/brandonkylely"
+            target="_blank" className=""><i class="fa-brands fa-github"></i></a>
+          <a href="https://www.linkedin.com/in/brandon-ly-7300b1205/"
+            target="_blank" className="mx-12">
+            <i class="fa-brands fa-linkedin"></i>
+          </a>
+          <a href="https://twitter.com/brandonkylely"
+            target="_blank" className="">
+            <i class="fa-brands fa-twitter"></i>
+          </a>
+          <br/>
+          
+          <a
+            href="https://brandonkylely.github.io/resume/"
+            target="_blank"
+            className="text-3xl"
+          >
+            Resume <i class="fa-solid fa-location-arrow"></i>
+          </a>
         </div>
-
-        </div>
-    )
+      </div>
+      <div className="font-bold text-3xl mt-12 col-start-2">
+        About Me
+        <br />
+        ━━━━━━━━━━━━━━━━━━━━━━━━━━
+        <p className="font-xl w-2/3 text-right ml-12">
+          Hi, I'm Brandon, and welcome to my portfolio.
+          <br />
+          <br />
+          I'm a Fullstack Software Engineer with a background in Biochemistry.
+          <br />
+          <br />
+          Feel free to contact me with any business inquiries!
+        </p>
+      </div>
+    </div>
+  );
 }
