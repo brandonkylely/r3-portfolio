205ed9ef129b42ec08746845dd8b6f37ea64d4ff
Brandon <brandonkly@ucla.edu>
2023-09-28 23:42:50 -0700

    added loading icon

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,6 +1,11 @@
 import { useProgress, Html } from "@react-three/drei";
+import "./Loading.css";
 
 export default function Loading() {
   const { active, progress, errors, item, loaded, total } = useProgress();
-  return <Html wrapperClass ="itimFont" center>{Math.round(progress)} % Loaded</Html>;
+  return <Html wrapperClass ="itimFont" center><div class="lds-facebook">
+  <div></div><div></div><div></div>
+</div>
+<br/>
+{Math.round(progress)} % Loaded</Html>;
 }
