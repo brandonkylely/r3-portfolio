959e4acd485451e4df5c465a753dfe5f027933b7
Brandon <brandonkly@ucla.edu>
2023-10-09 18:48:50 -0700

    gif sizing for mobile

--- src/components/pages/Demo.jsx
+++ src/components/pages/Demo.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Demo({}) {
   return (
-    <div className="flex flex-col items-center justify-center p-8">
+    <div className="w-800px flex flex-col items-center justify-center p-8">
       <div className="font-bold schoolbellFont italic text-3xl mb-6">Featured Works</div>
-      <img className="h-80 rounded" src="./demo.gif"></img>
+      <img className="w-10/12 rounded" src="./demo.gif"></img>
     </div>
   );
 }
