562b6cf6a12167baeac85e2cd741c794ebfba6c0
Brandon <brandonkly@ucla.edu>
2023-10-13 02:28:32 -0700

    state loading

--- src/components/pages/Demo.jsx
+++ src/components/pages/Demo.jsx
@@ -1,19 +1,29 @@
-import { Suspense } from "react";
+import { useState } from "react";
 import "../Loading.css";
 
-export default function Demo({}) {
+export default function Demo() {
+  const [loading, setLoading] = useState(true);
+  const handleSetLoad = () => setLoading(false);
   return (
     <div className="w-900px-min w-800px flex flex-col items-center justify-center p-8">
       <div className="font-bold itimFont italic text-3xl mb-6">
         Featured Works
       </div>
-      <Suspense fallback={(<div className="lds-facebook">
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>)}>
-        <img className="w-11/12 rounded" src="./demo.gif"></img>
-      </Suspense>
+      {loading ? (
+        <>
+          <img className="w-11/12 rounded invisible absolute" src="./demo.gif"
+          onLoad={handleSetLoad}></img>
+          <div className="lds-facebook">
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </>
+      ) : (
+        <img className="w-11/12 rounded" src="./demo.gif"
+        onLoad={handleSetLoad}></img>
+      )}
+
     </div>
   );
 }
