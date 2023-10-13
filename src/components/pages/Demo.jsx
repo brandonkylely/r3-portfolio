da660148dbd308c3d66b1e6864f89457906e2afb
Brandon <brandonkly@ucla.edu>
2023-10-13 01:24:49 -0700

    loading icon for demo gif

--- src/components/pages/Demo.jsx
+++ src/components/pages/Demo.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Suspense } from "react";
+import Loading from "../Loading";
 
 export default function Demo({}) {
   return (
     <div className="w-900px-min w-800px flex flex-col items-center justify-center p-8">
-      <div className="font-bold itimFont italic text-3xl mb-6">Featured Works</div>
-      <img className="w-11/12 rounded" src="./demo.gif"></img>
+      <div className="font-bold itimFont italic text-3xl mb-6">
+        Featured Works
+      </div>
+      <Suspense fallback={<Loading />}>
+        <img className="w-11/12 rounded" src="./demo.gif"></img>
+      </Suspense>
     </div>
   );
 }
